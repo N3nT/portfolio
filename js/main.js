03d8a98f3d4c294552f8f798e160b4008a9b12b2
N3nT <bartosz.widlak@op.pl>
2024-06-11 12:33:40 +0200

    About me section, numbers for it

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 const body = document.querySelector('body')
 const hamburger = document.querySelector('.fa-solid')
 const mobileNav = document.querySelector('.mobile-nav')
+const mobileNavListElements = document.querySelectorAll('.mobile-nav__list__list-element')
 
 const toggleMobileMenu = () => {
 	mobileNav.classList.toggle('mobile-nav--active')
 	hamburger.classList.toggle('fa-bars')
 	hamburger.classList.toggle('fa-xmark')
     body.classList.toggle('fixed-body')
+	
 }
 
 hamburger.addEventListener('click', toggleMobileMenu)
+mobileNavListElements.forEach(li => {
+	li.addEventListener('click', toggleMobileMenu)
+});
