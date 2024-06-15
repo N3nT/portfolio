cee8ddb7a35ca8d04598cae7c0cfebb3cc312771
N3nT <bar55v71@interia.pl>
2024-06-15 22:22:38 +0200

    Swiper autoplay, sticky menu, fixes on media

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const body = document.querySelector('body')
 const hamburger = document.querySelector('.fa-solid')
 const mobileNav = document.querySelector('.mobile-nav')
 const mobileNavListElements = document.querySelectorAll('.mobile-nav__list-element')
+const pcNav = document.querySelector('.nav__pc')
+const pcNavLine = document.querySelector('.pc-line')
 
 const toggleMobileMenu = () => {
 	mobileNav.classList.toggle('mobile-nav--active')
@@ -11,6 +13,19 @@ const toggleMobileMenu = () => {
 	
 }
 
+const stickyNav = () => {
+	//console.log(window.scrollY);
+	if(window.scrollY >= 83){
+		pcNav.classList.add('sticky-nav')
+		pcNavLine.classList.add('sticky-line')
+
+	} else {
+		console.log('dick2');
+		pcNav.classList.remove('sticky-nav')
+	}
+}
+
+window.addEventListener('scroll', stickyNav)
 hamburger.addEventListener('click', toggleMobileMenu)
 mobileNavListElements.forEach(li => {
 	li.addEventListener('click', toggleMobileMenu)
--- js/swiper.js
+++ js/swiper.js
@@ -2,6 +2,9 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    autoplay: {
+      delay: 5000,
+    },
     pagination: {
       el: '.swiper-pagination',
     }
