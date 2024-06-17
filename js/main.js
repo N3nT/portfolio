b560696f28ff8e218970b90cbe6107c36024aa3e
N3nT <bar55v71@interia.pl>
2024-06-17 12:42:13 +0200

    Performance fixes

--- js/main.js
+++ js/main.js
@@ -4,35 +4,32 @@ const mobileNav = document.querySelector('.mobile-nav')
 const mobileNavListElements = document.querySelectorAll('.mobile-nav__list-element')
 const pcNav = document.querySelector('.nav__pc')
 const pcNavLine = document.querySelector('.pc-line')
-const form = document.querySelector("form");
+const form = document.querySelector('form')
 
 const toggleMobileMenu = () => {
 	mobileNav.classList.toggle('mobile-nav--active')
 	hamburger.classList.toggle('fa-bars')
 	hamburger.classList.toggle('fa-xmark')
-    body.classList.toggle('fixed-body')
-	
+	body.classList.toggle('fixed-body')
 }
 
 const stickyNav = () => {
-	if(window.scrollY >= 83){
+	if (window.scrollY >= 83) {
 		pcNav.classList.add('sticky-nav')
-
 	} else {
-		console.log('dick2');
 		pcNav.classList.remove('sticky-nav')
 	}
 }
 
 if (form) {
-  form.addEventListener("submit", e => {
-    e.preventDefault();
-    e.currentTarget.submit();
-  });
+	form.addEventListener('submit', e => {
+		e.preventDefault()
+		e.currentTarget.submit()
+	})
 }
 
 window.addEventListener('scroll', stickyNav)
 hamburger.addEventListener('click', toggleMobileMenu)
 mobileNavListElements.forEach(li => {
 	li.addEventListener('click', toggleMobileMenu)
-});
+})
--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,4 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     direction: 'horizontal',
     loop: true,
     autoplay: {
